846f90e956475189cd6896605005db3e8cb53e58
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-17 10:16:42 +0300

    UpdateCreate

--- stock/models.py
+++ stock/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+class UpdateCreate(models.Model):
+    createds = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        abstract = True
+
 class Category(models.Model):
     name=models.CharField(max_length=25)
 
@@ -19,20 +28,19 @@ class Brand(models.Model):
     
 
     
-class Product(models.Model):
+class Product(UpdateCreate):
     name = models.CharField(max_length=100, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="b_products")
     stock = models.PositiveSmallIntegerField(blank=True, default=0)
-    createds = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+
 
     
     def __str__(self):
         return self.name
 
 
-class Firm(models.Model):
+class Firm(UpdateCreate):
     name = models.CharField(max_length=25, unique=True)
     phone = models.CharField(max_length=25)
     address = models.CharField(max_length=200)
@@ -42,7 +50,7 @@ class Firm(models.Model):
     def __str__(self):
         return self.name
     
-class Purchases(models.Model):
+class Purchases(UpdateCreate):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     firm = models.ForeignKey(Firm, on_delete=models.SET_NULL, null=True, related_name="purchases")
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, related_name="b_purchases")
@@ -57,7 +65,7 @@ class Purchases(models.Model):
 
 
 
-class Sales(models.Model):
+class Sales(UpdateCreate):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, related_name="b_sales")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="sale")

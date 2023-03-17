183f0ca4e2a996650dd409d811b661872c25d189
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-17 19:35:45 +0300

    permissions

--- stock/serializers.py
+++ stock/serializers.py
@@ -4,13 +4,36 @@ from .models import Category, Brand, Product, Firm, Purchases, Sales
 
 
 class CategorySerializer(serializers.ModelSerializer):
-  product_count = serializers.SerializerMethodField
+  product_count = serializers.SerializerMethodField()
 
   class Meta:
     model = Category
-    fields = ("id", "name") 
+    fields = ("id", "name", "product_count") 
 
 
   def  get_product_count(self, obj):
     return Product.objects.filter(category_id = obj.id).count()
+  
+class ProductSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Product
+    fields = "__all__"
+
+
+class CategoryProductSerializers(serializers.ModelSerializer):
+
+    products = ProductSerializer(many=True)
+    product_count = serializers.SerializerMethodField()
+   
+    class Meta:
+      model = Category
+      fields = ("id", "name", "product_count", "products") 
+
+
+    def  get_product_count(self, obj):
+       return Product.objects.filter(category_id = obj.id).count()
+    
       
+
+
+
--- stock/views.py
+++ stock/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Category, Brand, Product, Firm, Purchases, Sales
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer, CategoryProductSerializers
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import DjangoModelPermissions
 
 
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['username']
+    filterset_fields = ["name"]
+    permission_classes = [DjangoModelPermissions]
+
+    def get_serializer_class(self):
+        if self.request.query_params.get("name"):
+            return CategoryProductSerializers
+        return super().get_serializer_class()
+  

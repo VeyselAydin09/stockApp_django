08b2aa58270c52177382011a4dac0e609699bceb
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-17 16:06:23 +0300

    product_count

---
+++ stock/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Category, Brand, Product, Firm, Purchases, Sales
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+  product_count = serializers.SerializerMethodField
+
+  class Meta:
+    model = Category
+    fields = ("id", "name") 
+
+
+  def  get_product_count(self, obj):
+    return Product.objects.filter(category_id = obj.id).count()
+      
--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Brand, Product, Firm, Purchases, Sales
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer

93c64ed2cdb170d470703e7c3d53b25d6e349787
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-19 20:21:17 +0300

    brand urls

--- stock/urls.py
+++ stock/urls.py
@@ -5,7 +5,7 @@ from .views import CategoryView,BrandView
 
 router = routers.DefaultRouter()
 router.register("categories", CategoryView)
-router.register("brands", BrandView)
+router.register("brand", BrandView)
 
 
 urlpatterns = [

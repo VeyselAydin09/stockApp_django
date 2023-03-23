d0910aa677f707723afbf18d45808ed740a9b73e
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-23 22:07:35 +0300

    def perform

--- stock/views.py
+++ stock/views.py
@@ -57,3 +57,6 @@ class PurchaseView(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)  

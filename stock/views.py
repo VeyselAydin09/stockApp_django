fb2d6040aed842141a6f9ed43c9138e46413376e
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-24 20:30:16 +0300

    def update

--- stock/views.py
+++ stock/views.py
@@ -60,3 +60,9 @@ class PurchaseView(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)  
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)

214e4c04710b0ccea2c475b0111079e00eff46e8
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-28 19:13:33 +0300

    SalesView

--- stock/urls.py
+++ stock/urls.py
@@ -8,6 +8,8 @@ router.register("categories", CategoryView)
 router.register("brands", BrandView)
 router.register("firms", FirmView)
 router.register("purchases", PurchaseView)
+router.register("sales", SalesView)
+
 
 
 urlpatterns = [
